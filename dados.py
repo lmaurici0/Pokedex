d99a428982560cc8c390ea639dbd6acc3d69b19e
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2023-10-31 23:13:38 -0300

    Update dados.py
    Fix bugs in dictionary

--- dados.py
+++ dados.py
@@ -1,43 +1,43 @@
 pokemon = {
     'Pikachu' : {
         'status' : ['HP: 300', 'Ataque: 600', 'Defesa: 500','Velocidade: 300','Total: 1700'],
-        'habilidades': ['Choque do trovão', 'Cabeçada'],
+        'habilidades': ['Choque Do Trovão', 'Cabeçada'],
         'tipo': ['#025', 'Elétrico', 'images/pikachu.png', '#FCC719'],
 },
 
      'Bulbasaur' : {
         'status' : ['HP: 320', 'Ataque: 510', 'Defesa: 400', 'Velocidade: 200', 'Total: 1.430'],
-        'habilidades': ['Folhas navalha','Raio solar'],
+        'habilidades': ['Folhas Navalha','Raio Solar'],
         'tipo': ['#001', 'Grama', 'images/bulbasaur.png','#49D0B0'],
 },
 
      'Charmander' : {
         'status' : ['HP: 200','Ataque: 300','Defesa: 400','Velocidade: 320','Total: 1.220'],
-        'habilidades': ['Lança-chamas','Ataque rápido'],
+        'habilidades': ['Lança-Chamas','Ataque Rápido'],
         'tipo': ['#004','fogo','images/charmander.png','#ED8A8B'],
 },
 
      'Dragonite' : {
-        'status' : ['HP: 500','Ataque: 600','Velocidade: 400','Total: 1.900'],
-        'habilidades': ['Dança do dragão','Velocidade extrema'],
+        'status' : ['HP: 500', 'Ataque: 600', 'Defesa: 95', 'Velocidade: 400','Total: 1.900'],
+        'habilidades': ['Dança Do Dragão','Velocidade Extrema'],
         'tipo': ['#149','dragão','images/dragonite.png', '#C29791'],
 },
 
      'Gengar' : {
-        'status' : ['HP: 100','Ataque: 200','Defesa: 300','Velocidade: 400','Total: 1.000'],
-        'habilidades': ['Bola sombria','Lambida'],
+        'status' : ['HP: 100', 'Ataque: 200', 'Defesa: 300', 'Velocidade: 400', 'Total: 1.000'],
+        'habilidades': ['Bola Sombria','Lambida'],
         'tipo': ['#094','fantasma','images/gengar.png', '#BA68C8'],
 },
 
      'Gyarados' : {
         'status' : ['HP: 300','Ataque: 600','Defesa: 500','Velocidade: 300','Total: 1.700'],
-        'habilidades': ["Jato d'Água",'Hidro bomba'],
+        'habilidades': ["Jato d'Água",'Hidro Bomba'],
         'tipo': ['#130','água','images/gyarados.png', '#76BEFE'],
 },
 
      'Mewtwo':{
         'status' : ['HP: 160','Ataque: 110','Defesa: 90','Velocidade: 130','Total: 490'],
-        'habilidades': ["Controle mental","Teleporte"],
+        'habilidades': ["Controle Mental","Teleporte"],
         'tipo': ['#150', 'Psíquico', 'images/mewtwo.png', '#B696B7'],
 },
 
@@ -49,7 +49,7 @@ pokemon = {
 
      'Arceus':{
         'status': ['HP: 120','Ataque: 120','Defesa: 120','Velocidade: 120','Total: 480'],   
-        'habilidades': ['manipulação da realidade ','manipulação dimensional'],
+        'habilidades': ['Manipulação Da realidade' ,'Manipulação Dimensional'],
         'tipo': ['#493','Normal','images/arceus.png','#E6E6E6'],        
 },
 
