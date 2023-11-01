285225ba2adc1d9a550805655ac336176574be76
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2023-10-31 23:16:47 -0300

    Update main.py
    Fix bug in Arceus button and in the Dragonite Button=

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from PIL import Image,ImageTk
 from dados import *
 import random
 
-# cores
-co0 = "#444466"  # Preta
-co1 = "#feffff"  # branca
-co2 = "#6f9fbd"  # azul
-co3 = "#38576b"  # valor
-co4 = "#403d3d"   # letra
-co5 = "#ef5350"   # vermelha
+#  Colors
+co0 = "# 444466"     #  Black
+co1 = "# feffff"     #  White
+co2 = "# 6f9fbd"     #  Blue
+co3 = "# 38576b"     #  Value
+co4 = "# 403d3d"     #  letter
+co5 = "# ef5350"     #  Red
 
 janela = Tk()
-janela.title('')
+janela.title('Pokédex')
+janela.iconbitmap("images/pokeball.ico")
 janela.geometry('700x600')
 janela.configure(bg=co1)
 
@@ -22,7 +23,7 @@ ttk.Separator(janela, orient=HORIZONTAL).grid(row=0, columnspan=1, ipadx=272)
 style = ttk.Style(janela)
 style.theme_use('clam')
 
-#criando frame
+# Frame Creation
 frame_pokemon = Frame(janela, width=700, height=290, relief='flat')
 frame_pokemon.grid(row=1, column=0)
 
@@ -30,7 +31,7 @@ def troca_pokemon(i):
     global img_pok, pok_img
     frame_pokemon['bg'] = pokemon[i]['tipo'][3] 
 
-    #tipo de pokemon
+    # Pokemon Type
     pok_nome['text'] =  [i]
     pok_nome['bg'] = pokemon[i] ['tipo'][3]
     pok_tipo['text'] = pokemon[i] ['tipo'][1]
@@ -54,54 +55,55 @@ def troca_pokemon(i):
     pok_hb1['text'] = pokemon[i]['habilidades'][0]
     pok_hb2['text'] = pokemon[i]['habilidades'][1]
 
-# nomes
+# Names
 pok_nome = Label(frame_pokemon, text='', relief='flat', anchor=CENTER, font=('Fixedsys 20'), fg=co1)
 pok_nome.place(x=12,y=15)
 
-#categoria
+# Cattegoriy
 pok_tipo = Label(frame_pokemon, text='', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), bg=co1, fg=co1)
 pok_tipo.place(x=12,y=50 )
 
-#id
+# Id
 pok_id = Label(frame_pokemon, text='', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), bg=co1, fg=co1)
 pok_id.place(x=12,y=75)
 
-#status
+# Stats
 pok_stts = Label(janela, text='Status', relief='flat', anchor=CENTER, font=('Verdana 20'), bg=co1, fg=co0)
 pok_stts.place(x=15,y=310 )
-#hp
-pok_hp = Label(janela, text='HP: 300', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+
+# Hp
+pok_hp = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pok_hp.place(x=15,y=360)
 
-#ataque
-pok_atack = Label(janela, text='Ataque: 600', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+# Attack
+pok_atack = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pok_atack.place(x=15,y=385)
 
-#defesa
-pok_def = Label(janela, text='Defesa: 500', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+# Defense
+pok_def = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pok_def.place(x=15,y=410)
 
-#velocidade
-pok_vel = Label(janela, text='Velocidade: 300', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+# Velocity
+pok_vel = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pok_vel.place(x=15,y=435)
 
-#total
-pok_tot = Label(janela, text='Total: 1.700', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+# Total
+pok_tot = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pok_tot.place(x=15,y=460)
 
-#habilidades
-pok_habilidade= Label(janela, text='Habilidades', relief='flat', anchor=CENTER, font=('Verdana 20'), bg=co1, fg=co0)
+# Habilities
+pok_habilidade= Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 20'), bg=co1, fg=co0)
 pok_habilidade.place(x=180,y=310 )
 
-#hp
-pok_hb1 = Label(janela, text='Choque do trovão', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+# Hp
+pok_hb1 = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pok_hb1.place(x=195,y=360)
 
-#ataque
-pok_hb2 = Label(janela, text='Cabeçada', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+# Attack
+pok_hb2 = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pok_hb2.place(x=195,y=385)
 
-# Criando botoes
+# Buttons
 img_pok_1 = Image.open('images/cabeca-pikachu.png')
 img_pok_1 = img_pok_1.resize((40, 40))
 img_pok_1 = ImageTk.PhotoImage(img_pok_1)
@@ -160,7 +162,7 @@ img_pok_10 = Image.open('images/cabeca-arceus.png')
 img_pok_10 = img_pok_10.resize((40, 40))
 img_pok_10 = ImageTk.PhotoImage(img_pok_10)
 b_pok_10 = Button(janela,command=lambda:troca_pokemon('Arceus'), image=img_pok_10, text='Arceus', width=150, relief='raised', overrelief=RIDGE, compound=LEFT, anchor=NW, padx=5, font=('Verdana 12'), bg=co1, fg=co0)
-b_pok_10.place(x=700,y=540)
+b_pok_10.place(x=500,y=540)
 
 random_pokemon = ['Dragonite', 'Gengar', 'Gyarados', 'Charmander', 'Pikachu', 'Bulbasaur', 'Mewtwo', 'Mew', 'Magikarp', 'Arceus']
 poke_final = random.sample(random_pokemon, 1)
